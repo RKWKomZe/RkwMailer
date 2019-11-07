429007c19e314f810c0651cf691fee03d56b8588
Steffen Kroggel <kroggel@rkw.de>
2019-11-07 12:27:30 +0100

    [REFACTORING] user_func for ext_localconf.php and ext_tables.php / fix for flashMessages

--- ext_tables.php
+++ ext_tables.php
@@ -1,54 +1,75 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-if (TYPO3_MODE === 'BE') {
+call_user_func(
+    function($extKey)
+    {
 
-	/**
-	 * Registers a Backend Module
-	 */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'RKW.' . $_EXTKEY,
-		'tools',	 // Make module a submodule of 'Web'
-		'mailadministration',	// Submodule key
-		'',						// Position
-		array(
-			'Backend' => 'statistics, clickStatistics, list, pause, continue, delete, reset',
-		),
-		array(
-			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_backend.xlf',
-		)
-	);
+        //=================================================================
+        // Register BackendModule
+        //=================================================================
+        if (TYPO3_MODE === 'BE') {
 
-}
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'RKW.' . $extKey,
+                'tools',	 // Make module a submodule of 'Web'
+                'mailadministration',	// Submodule key
+                '',						// Position
+                array(
+                    'Backend' => 'statistics, clickStatistics, list, pause, continue, delete, reset',
+                ),
+                array(
+                    'access' => 'user,group',
+                    'icon'   => 'EXT:' . $extKey . '/ext_icon.gif',
+                    'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_backend.xlf',
+                )
+            );
+        }
+
+        //=================================================================
+        // Register Plugin
+        //=================================================================
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'RKW.' . $extKey,
+            'Rkwmailer',
+            'RKW Mailer'
+        );
+
+        //=================================================================
+        // Add TypoScript
+        //=================================================================
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript',
+            'RKW Mailer'
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwmailer',
-	'RKW Mailer'
+        //=================================================================
+        // Add tables
+        //=================================================================
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwmailer_domain_model_link'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwmailer_domain_model_link'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwmailer_domain_model_queuemail'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwmailer_domain_model_queuerecipient'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwmailer_domain_model_statisticopening'
+        );
+
+    },
+    $_EXTKEY
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'RKW Mailer');
 
-
-//=================================================================
-// Add tables
-//=================================================================
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwmailer_domain_model_link', 'EXT:rkw_mailer/Resources/Private/Language/locallang_csh_tx_rkwmailer_domain_model_link.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwmailer_domain_model_link');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwmailer_domain_model_link', 'EXT:rkw_mailer/Resources/Private/Language/locallang_csh_tx_rkwmailer_domain_model_link.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwmailer_domain_model_link');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwmailer_domain_model_queuemail', 'EXT:rkw_mailer/Resources/Private/Language/locallang_csh_tx_rkwmailer_domain_model_queuemail.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwmailer_domain_model_queuemail');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwmailer_domain_model_queuerecipient', 'EXT:rkw_mailer/Resources/Private/Language/locallang_csh_tx_rkwmailer_domain_model_queuerecipient.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwmailer_domain_model_queuerecipient');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwmailer_domain_model_statisticopening', 'EXT:rkw_mailer/Resources/Private/Language/locallang_csh_tx_rkwmailer_domain_model_statisticopening.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwmailer_domain_model_statisticopening');
