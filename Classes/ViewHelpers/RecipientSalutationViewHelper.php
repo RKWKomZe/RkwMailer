ffc2397facb9d810dc88de76865691019f765843
Maximilian Faessler <maximilian@faesslerweb.de>
2020-04-25 13:14:46 +0000

    [EDIT] make RecipientSalutation VH t87 compatible

--- Classes/ViewHelpers/RecipientSalutationViewHelper.php
+++ Classes/ViewHelpers/RecipientSalutationViewHelper.php
@@ -15,106 +15,199 @@ namespace RKW\RkwMailer\ViewHelpers;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 use \TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
-use \TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
 
-/**
- * Class RecipientSalutationViewHelper
- *
- * @author Maximilian Fäßler <maximilian@faesslerweb.de>
- * @author Steffen Kroggel <developer@steffenkroggel.de>
- * @copyright Rkw Kompetenzzentrum
- * @package RKW_RkwMailer
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- */
-class RecipientSalutationViewHelper extends AbstractViewHelper implements CompilableInterface
-{
-
+$currentVersion = VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version);
+if ($currentVersion <= 8000000) {
     /**
-     * Build a full salutation for the queueRecipient
+     * Class RecipientSalutationViewHelper
      *
-     * @param \RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient
-     * @param bool $useFirstName
-     * @param string $prependText
-     * @param string $appendText
-     * @param string $fallbackText
-     * @return string $string
+     * @deprecated For TYPO3 7.6 only
+     *
+     * @author Maximilian Fäßler <maximilian@faesslerweb.de>
+     * @author Steffen Kroggel <developer@steffenkroggel.de>
+     * @copyright Rkw Kompetenzzentrum
+     * @package RKW_RkwMailer
+     * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
      */
-    public function render(\RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient, $useFirstName = false, $prependText = '', $appendText = '', $fallbackText = '')
+    class RecipientSalutationViewHelper extends AbstractViewHelper implements CompilableInterface
     {
 
-        return static::renderStatic(
-            array(
-                'queueRecipient' => $queueRecipient,
-                'useFirstName'   => $useFirstName,
-                'appendText'     => $appendText,
-                'prependText'    => $prependText,
-                'fallbackText'   => $fallbackText
-            ),
-            $this->buildRenderChildrenClosure(),
-            $this->renderingContext
-        );
-        //===
-    }
+        /**
+         * Build a full salutation for the queueRecipient
+         *
+         * @param \RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient
+         * @param bool                                       $useFirstName
+         * @param string                                     $prependText
+         * @param string                                     $appendText
+         * @param string                                     $fallbackText
+         * @return string $string
+         */
+        public function render(\RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient, $useFirstName = false, $prependText = '', $appendText = '', $fallbackText = '')
+        {
 
-
-    /**
-     * Static rendering
-     *
-     * @param array $arguments
-     * @param \Closure $renderChildrenClosure
-     * @param RenderingContextInterface $renderingContext
-     * @return string
-     */
-    static public function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
-    {
-        /** @var \RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient */
-        $queueRecipient = $arguments['queueRecipient'];
-        $useFirstName = $arguments['useFirstName'];
-        $appendText = $arguments['appendText'];
-        $prependText = $arguments['prependText'];
-        $fallbackText = $arguments['fallbackText'];
-
-        $fullName = array();
-        if ($queueRecipient->getLastName()) {
-
-            if ($queueRecipient->getSalutationText()) {
-                $fullName[] = $queueRecipient->getSalutationText();
-            } else {
-                $useFirstName = true;
-            }
-
-            if ($queueRecipient->getTitle()) {
-                $fullName[] = $queueRecipient->getTitle();
-            }
-
-            if (
-                ($useFirstName == true)
-                && ($queueRecipient->getFirstName())
-            ) {
-                $fullName[] = ucFirst($queueRecipient->getFirstName());
-            }
-
-            $fullName[] = ucFirst($queueRecipient->getLastName());
-        }
-
-
-        $finalName = trim(implode(' ', $fullName));
-        if (!$finalName) {
-
-            if ($fallbackText) {
-                return $fallbackText;
-                //===
-            }
-
-            return trim(($prependText ? $prependText : '')) . ($appendText ? $appendText : '');
+            return static::renderStatic(
+                array(
+                    'queueRecipient' => $queueRecipient,
+                    'useFirstName'   => $useFirstName,
+                    'appendText'     => $appendText,
+                    'prependText'    => $prependText,
+                    'fallbackText'   => $fallbackText
+                ),
+                $this->buildRenderChildrenClosure(),
+                $this->renderingContext
+            );
             //===
         }
 
-        return ($prependText ? $prependText : '') . trim(implode(' ', $fullName)) . ($appendText ? $appendText : '');
-        //===
+
+        /**
+         * Static rendering
+         *
+         * @param array                     $arguments
+         * @param \Closure                  $renderChildrenClosure
+         * @param \TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface $renderingContext
+         * @return string
+         */
+        static public function renderStatic(array $arguments, \Closure $renderChildrenClosure, \TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface $renderingContext)
+        {
+            /** @var \RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient */
+            $queueRecipient = $arguments['queueRecipient'];
+            $useFirstName = $arguments['useFirstName'];
+            $appendText = $arguments['appendText'];
+            $prependText = $arguments['prependText'];
+            $fallbackText = $arguments['fallbackText'];
+
+            $fullName = array();
+            if ($queueRecipient->getLastName()) {
+
+                if ($queueRecipient->getSalutationText()) {
+                    $fullName[] = $queueRecipient->getSalutationText();
+                } else {
+                    $useFirstName = true;
+                }
+
+                if ($queueRecipient->getTitle()) {
+                    $fullName[] = $queueRecipient->getTitle();
+                }
+
+                if (
+                    ($useFirstName == true)
+                    && ($queueRecipient->getFirstName())
+                ) {
+                    $fullName[] = ucFirst($queueRecipient->getFirstName());
+                }
+
+                $fullName[] = ucFirst($queueRecipient->getLastName());
+            }
+
+
+            $finalName = trim(implode(' ', $fullName));
+            if (!$finalName) {
+
+                if ($fallbackText) {
+                    return $fallbackText;
+                    //===
+                }
+
+                return trim(($prependText ? $prependText : '')) . ($appendText ? $appendText : '');
+                //===
+            }
+
+            return ($prependText ? $prependText : '') . trim(implode(' ', $fullName)) . ($appendText ? $appendText : '');
+            //===
+        }
+
+
     }
+} else {
+    /**
+     * Class RecipientSalutationViewHelper
+     *
+     * For Typo3 >= 8.7
+     *
+     * @author Maximilian Fäßler <maximilian@faesslerweb.de>
+     * @author Steffen Kroggel <developer@steffenkroggel.de>
+     * @copyright Rkw Kompetenzzentrum
+     * @package RKW_RkwMailer
+     * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+     */
+    class RecipientSalutationViewHelper extends AbstractViewHelper implements CompilableInterface
+    {
+
+        /**
+         * initializeArguments
+         */
+        public function initializeArguments()
+        {
+            parent::initializeArguments();
+            $this->registerArgument('queueRecipient', '\RKW\RkwMailer\Domain\Model\QueueRecipient', 'The queue recipient', true);
+            $this->registerArgument('useFirstName', 'bool', 'Set to true if first name should be used in salutation', false, false);
+            $this->registerArgument('appendText', 'string', 'Set text you want to append to the salutation', false, '');
+            $this->registerArgument('prependText', 'string', 'Set text you want to prepend to the salutation', false, '');
+            $this->registerArgument('fallbackText', 'string', 'Set text you want to use as general fallback', false, '');
+        }
 
 
+        /**
+         * Static rendering
+         *
+         * @param array                     $arguments
+         * @param \Closure                  $renderChildrenClosure
+         * @param \TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext
+         * @return string
+         */
+        static public function renderStatic(array $arguments, \Closure $renderChildrenClosure, \TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext)
+        {
+            /** @var \RKW\RkwMailer\Domain\Model\QueueRecipient $queueRecipient */
+            $queueRecipient = $arguments['queueRecipient'];
+            $useFirstName = $arguments['useFirstName'];
+            $appendText = $arguments['appendText'];
+            $prependText = $arguments['prependText'];
+            $fallbackText = $arguments['fallbackText'];
+
+            $fullName = array();
+            if ($queueRecipient->getLastName()) {
+
+                if ($queueRecipient->getSalutationText()) {
+                    $fullName[] = $queueRecipient->getSalutationText();
+                } else {
+                    $useFirstName = true;
+                }
+
+                if ($queueRecipient->getTitle()) {
+                    $fullName[] = $queueRecipient->getTitle();
+                }
+
+                if (
+                    ($useFirstName == true)
+                    && ($queueRecipient->getFirstName())
+                ) {
+                    $fullName[] = ucFirst($queueRecipient->getFirstName());
+                }
+
+                $fullName[] = ucFirst($queueRecipient->getLastName());
+            }
+
+
+            $finalName = trim(implode(' ', $fullName));
+            if (!$finalName) {
+
+                if ($fallbackText) {
+                    return $fallbackText;
+                    //===
+                }
+
+                return trim(($prependText ? $prependText : '')) . ($appendText ? $appendText : '');
+                //===
+            }
+
+            return ($prependText ? $prependText : '') . trim(implode(' ', $fullName)) . ($appendText ? $appendText : '');
+            //===
+        }
+
+
+    }
 }
